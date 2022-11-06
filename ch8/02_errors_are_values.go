3eb4308c2736829d0fed96a0e06b11b9e4cbd9d5
aibazhang <dtjp@tutanota.com>
2022-11-06 23:36:43 +0900

    add: ch10 channel

---
+++ ch8/02_errors_are_values.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Status int64
+
+const (
+	InvalidLogin = iota + 1
+	NotFound
+)
+
+type StatusErr struct {
+	Status  Status
+	Message string
+}
+
+func (se StatusErr) Error() string {
+	return se.Message
+}
+
+// func LoginAndGetData(uid, pwd, file string) ([]byte, error) {
+// 	err := login(uid, pwd)
+// 	if err != nil {
+// 		return nil, StatusErr{
+// 			Status:  NotFound,
+// 			Message: fmt.Sprintf("invalid credentials for user %s", uid),
+// 		}
+// 	}
+// 	data, err := getData(file)
+// 	if err != nil {
+// 		return nil, StatusErr{
+// 			Status:  NotFound,
+// 			Message: fmt.Sprintf("file %s not found", file),
+// 		}
+// 		return data, nil
+// 	}
+// }
+
+func GenerateError(flag bool) error {
+	var genErr error
+	if flag {
+		genErr = StatusErr{
+			Status: NotFound,
+		}
+	}
+	return genErr
+}
+
+func main() {
+	err := GenerateError(true)
+	fmt.Println(err != nil)
+	err = GenerateError(false)
+	fmt.Println(err != nil)
+}
---
+++ ch9/02_working_with_versions/go.mod
@@ -0,0 +1,8 @@
+module example.com/region_tax
+
+go 1.19
+
+require (
+	github.com/learning-go-book/simpletax v1.0.0
+	github.com/shopspring/decimal v1.3.1
+)
